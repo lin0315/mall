6fb299b4185b7a02b5e9d6f13dfc562ec0bd3234
lin <1845948316@qq.com>
2021-01-11 21:38:30 +0800

    网络封装

---
+++ src/network/request.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+export function request(config) {
+  // 1. 创建axios的实例对象
+  const instance = axios.create({
+    baseURL: 'http://152.136.185.210:8000/api/w6',
+    timeout: 5000
+  })
+
+  // 请求拦截
+  instance.interceptors.request.use(config => {
+    return config
+  }, err => {
+
+  })
+
+  // 响应拦截
+  instance.interceptors.response.use(res => {
+    return res.data
+  }, err => {
+    console.log(err);
+  })
+  return instance(config)
+}
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -9,10 +9,24 @@
 <script>
 import NavBar from "components/common/navbar/NavBar";
 
+import { getHomeMultidata } from "network/home";
+
 export default {
   name: "Home",
   components: {
     NavBar,
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+    };
+  },
+  created() {
+    getHomeMultidata().then((res) => {
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    });
   },
 };
 </script>
